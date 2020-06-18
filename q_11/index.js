5cd0dc7538fc1b464d558028c097ab02aa5c7c80
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-06-18 15:35:10 -0500

    added alert function

--- q_11/index.js
+++ q_11/index.js
@@ -4,12 +4,12 @@ let celcius = document.querySelector('celcius')
 
 function fahrenheitConverter(input){
    let fahr = (input * 9/5) + 32
-    return console.log(fahr);
+    return alert(fahr);
 }
 
 function celciusConverter(input){
     let celc = (input - 32) / 1.8
-    return console.log(celc);
+    return alert(celc);
 }
 let fahrResult = fahrenheitConverter(temp)
 let celciusResult = celciusConverter(temp)
