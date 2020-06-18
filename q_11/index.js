0af7ed25268e0ce5750dfb13f692efd3866e93c1
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-06-18 15:32:18 -0500

    made my converter

--- q_11/index.js
+++ q_11/index.js
@@ -1,17 +1,18 @@
-let celcius = document.querySelector('celcius')
 let fahrenheit = document.querySelector('fahrenheit')
 let temp = document.querySelector('temperature')
 
 function fahrenheitConverter(input){
    let fahr = (input * 9/5) + 32
-    return fahr
+    return console.log(fahr);
 }
 
 function celciusConverter(input){
     let celc = (input - 32) / 1.8
-    return celc
+    return console.log(celc);
 }
+let fahrResult = fahrenheitConverter(temp)
+let celciusResult = celciusConverter(temp)
 
-fahrenheit.addEventListener('click', fahrenheitConverter)
+fahrenheit.addEventListener('click', fahrResult)
 
-celcius.addEventListener('click', celciusConverter)
+celcius.addEventListener('click', celciusResult)
