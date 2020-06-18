990bebeb3f63eeb6bf60aa6b8d0649913c87832d
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-06-18 15:34:26 -0500

    forgot to add celcius button

--- q_11/index.js
+++ q_11/index.js
@@ -1,5 +1,6 @@
 let fahrenheit = document.querySelector('fahrenheit')
 let temp = document.querySelector('temperature')
+let celcius = document.querySelector('celcius')
 
 function fahrenheitConverter(input){
    let fahr = (input * 9/5) + 32
